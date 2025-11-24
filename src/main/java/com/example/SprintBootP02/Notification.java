8ed2eb2e2001d5f10527d4fb1a36a5b2bc4e423f
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-24 11:46:05 +0530

    Commit

---
+++ src/main/java/com/example/SprintBootP02/Notification.java
@@ -0,0 +1,22 @@
+package com.example.SprintBootP02;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Notification {
+
+    @Autowired
+    EmailService emailService;
+
+
+    public Notification(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+
+    void Display(){
+        String str=emailService.sendEmail();
+        System.out.println(str);
+    }
+}
